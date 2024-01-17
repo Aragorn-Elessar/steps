ed9521a701bf7dbb40444a76c5a221821fec0e21
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2024-01-17 13:54:38 +0200

    feat: Implement useState hook to update the component view

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const messages = [
   'Learn React ⚛️',
   'Apply for jobs 💼',
@@ -5,12 +7,20 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 2;
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    step > 1 && setStep(step - 1);
+  }
+
+  function handleNext() {
+    step < 3 && setStep(step + 1);
+  }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={step >= 1 ? 'active' : ''}>1</div>
         <div className={step >= 2 ? 'active' : ''}>2</div>
         <div className={step >= 3 ? 'active' : ''}>3</div>
       </div>
@@ -20,10 +30,16 @@ export default function App() {
       </p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
